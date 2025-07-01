ec4f61c11eea93e0b967c61ee29fccecc4b84ad0
ArpanBarua <arpanbarua796@gmail.com>
2025-07-01 18:46:59 +0600

    file edited

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let searchbtn = document.querySelector(".search_icon");
+let searchbox = document.querySelector("#search-box");
+let crossbtn = document.querySelector(".cross");
+
+searchbtn.addEventListener('click', function(){
+    searchbox.classList.toggle('search_active');
+    
+});
+
+crossbtn.addEventListener('click', function(){
+    searchbox.classList.remove('search_active');
+   
+})
+ 
