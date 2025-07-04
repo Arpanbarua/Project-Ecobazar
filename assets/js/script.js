672b11abfd0eee25755692c3de4e164feca02145
ArpanBarua <arpanbarua796@gmail.com>
2025-07-05 01:34:21 +0600

    file modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,54 @@
-let searchbtn = document.querySelector(".search_icon");
-let searchbox = document.querySelector("#search-box");
-let crossbtn = document.querySelector(".cross");
+// $(function () {
+//     let searchbtn = document.querySelector(".search_icon");
+//     let searchbox = document.querySelector("#search-box");
+//     let crossbtn = document.querySelector(".cross");
 
-searchbtn.addEventListener('click', function(){
-    searchbox.classList.toggle('search_active');
-    
+//     searchbtn.addEventListener('click', function () {
+//         searchbox.classList.toggle('search_active');
+
+//     });
+
+//     crossbtn.addEventListener('click', function () {
+//         searchbox.classList.remove('search_active');
+
+//     });
+// });
+
+
+
+$(function () {
+
+    document.body.style.overflow = 'hidden';
+    let searchbtn = $('.search_icon');
+    let searchbox = $('#search-box');
+    let crossbtn = $('.cross');
+
+    searchbtn.on('click', function () {
+        searchbox.toggleClass('search_active');
+    });
+
+    crossbtn.on('click', function () {
+        searchbox.removeClass('search_active');
+    });
+
+
+    // Banner part for sliding
+
+    $('.sliders').slick({
+        dots: true,
+        slidesToShow: 1,
+        arrows: true, 
+        prevArrow: `<span class="prev"><iconify-icon icon="solar:arrow-left-linear" width="24" height="24"></iconify-icon></span>` ,
+        nextArrow: `<span class="next"><iconify-icon icon="mynaui:arrow-right" width="24" height="24"></iconify-icon></span>`,
+        autoplay: true,
+        autoplaySpeed: 3000,
+
+    });
+
+
+
 });
 
-crossbtn.addEventListener('click', function(){
-    searchbox.classList.remove('search_active');
-   
-});
 
 
- 
+
